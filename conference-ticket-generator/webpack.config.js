be49821324e15b7e1c3cd58cf955f30c5383aa6d
belousmind <belousmind@gmail.com>
2025-01-19 16:53:57 +0300

    feat: add style for button and input

--- conference-ticket-generator/webpack.config.js
+++ conference-ticket-generator/webpack.config.js
@@ -1,14 +1,20 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    assetModuleFilename: 'assets/[name][ext]'
   },
   module: {
     rules: [
+      {
+        test: /\.(png|jpe?g|gif|svg|ico)$/,
+        type: 'asset/resource',
+      },
       {
         test: /\.(woff|woff2|ttf|otf|eot)$/,
         type: 'asset/resource',
@@ -30,6 +36,11 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
+    new CopyPlugin({
+      patterns: [
+        { from: 'src/assets', to: 'assets' }, // Копирует папку assets
+      ],
+    })
   ],
   mode: 'development',
 }
