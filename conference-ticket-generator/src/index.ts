3d95bc359fbcc91427d5b53ac71f8cb18610dbb1
belousmind <belousmind@gmail.com>
2025-01-27 14:58:23 +0300

    feat: add uploading image by drag and drop

--- conference-ticket-generator/src/index.ts
+++ conference-ticket-generator/src/index.ts
@@ -1,89 +1,101 @@
 import './styles/style.scss';
 
-const submitForm = document.querySelector<HTMLInputElement>('input[type="submit"]');
+const submitForm = document.querySelector('input[type="submit"]') as HTMLInputElement;
 
 const formHandler = (event: Event): void => {
   event.preventDefault();
 };
 
-submitForm?.addEventListener('click', (event): void => formHandler(event));
+submitForm.addEventListener('click', (event): void => formHandler(event));
 
 let fileUrl: string | null = null;
 
-const fileInput = document.querySelector<HTMLInputElement>('#file-load');
-const hintText = document.querySelector<HTMLSpanElement>('.hint-text');
-const hint = document.querySelector<HTMLParagraphElement>('.hint');
+const fileInput = document.querySelector('#file-load') as HTMLInputElement;
+const hintText = document.querySelector('.hint-text') as HTMLSpanElement;
+const hint = document.querySelector('.hint') as HTMLParagraphElement;
 
-const uploadIcon = document.querySelector<HTMLImageElement>('.upload-icon');
-const instructionText = document.querySelector<HTMLParagraphElement>('.instruction-text');
+const uploadIcon = document.querySelector('.upload-icon') as HTMLImageElement;
+const instructionText = document.querySelector('.instruction-text') as HTMLParagraphElement;
 
-const imgControlsBtns = document.querySelector<HTMLDivElement>('.img-controls-btns');
-const uploadImg = document.querySelector<HTMLImageElement>('.upload-img');
+const imgControlsBtns = document.querySelector('.img-controls-btns') as HTMLDivElement;
+const uploadImg = document.querySelector('.upload-img') as HTMLImageElement;
 
-const removeImgBtn = document.querySelector<HTMLButtonElement>('.remove-img');
+const removeImgBtn = document.querySelector('.remove-img') as HTMLButtonElement;
 
-const changeImgBtn = document.querySelector<HTMLButtonElement>('.change-img');
+const changeImgBtn = document.querySelector('.change-img') as HTMLButtonElement;
 
-if (fileInput && hintText && uploadImg) {
-  fileInput.addEventListener('change', (): void => {
+const field = document.querySelector('.field') as HTMLDivElement;
 
-    const file = fileInput.files?.[0]; 
+const active = (): void => field?.classList.add('hover');
+const inactive = (): void => field?.classList.remove('hover');
 
-    if (!file) {
-      return;
-    }
+const uploadFile = (file: File) => {
+  if (file.size > 500 * 1024) {
+    hint.classList.add('warning');
+    hintText.textContent = 'File too large. Please upload a photo under 500KB.';
+    return;
+  }
 
-    if (file.size > 500 * 1024) {
-      hint?.classList.add('warning');
-      hintText.textContent = 'File too large. Please upload a photo under 500KB.';
-      return;
-    }
+  hint.classList.remove('warning');
+  hintText.textContent = 'Upload your photo (JPG or PNG, max size: 500KB).';
 
-    hint?.classList.remove('warning');
-    hintText.textContent = 'Upload your photo (JPG or PNG, max size: 500KB).';
+  uploadIcon.classList.add('hidden');
+  instructionText?.classList.add('hidden');
 
-    uploadIcon?.classList.add('hidden');
-    instructionText?.classList.add('hidden');
+  imgControlsBtns.classList.remove('hidden');
+  uploadImg.classList.remove('hidden');
 
-    imgControlsBtns?.classList.remove('hidden');
-    uploadImg.classList.remove('hidden');
-
-    fileUrl = URL.createObjectURL(file);
-    uploadImg.src = fileUrl;
- 
-  });
-
-  removeImgBtn?.addEventListener('click', (event): void => {
-    event.preventDefault();
-
-    uploadIcon?.classList.remove('hidden');
-    instructionText?.classList.remove('hidden');
-
-    imgControlsBtns?.classList.add('hidden');
-    uploadImg.classList.add('hidden');
-
-    uploadImg.src = '';
-  });
-
-  changeImgBtn?.addEventListener('click', (event): void => {
-    event.preventDefault();
-    fileInput.click();
-  });
+  fileUrl = URL.createObjectURL(file);
+  uploadImg.src = fileUrl;
 }
 
-const inputFullname = document.querySelector<HTMLInputElement>('#fullname');
-const inputEmail = document.querySelector<HTMLInputElement>('#email-input');
-const inputGithub = document.querySelector<HTMLInputElement>('#github-input');
+const prevents = (e: Event): void => e.preventDefault();
 
-inputFullname?.addEventListener('change', (event: Event):void => {
-  const target = event.target as HTMLInputElement;
-  console.log(target.value); // Выводим текущее значение
-})
+['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eName => {
+  field.addEventListener(eName, prevents);
+});
 
-inputEmail?.addEventListener('change', ():void => {
-  
-})
+['dragenter', 'dragover'].forEach(evtName => {
+  field.addEventListener(evtName, active);
+});
 
-inputGithub?.addEventListener('change', ():void => {
-  
-})
+['dragleave', 'drop'].forEach(evtName => {
+  field.addEventListener(evtName, inactive);
+});
+
+field.addEventListener('drop', (e):void => {
+  const file = e.dataTransfer?.files[0];
+  const acceptedTypes = ["image/png", "image/jpeg", "image/jpg"];
+  if (file && acceptedTypes.includes(file.type)) {
+    uploadFile(file);
+  }
+});
+
+fileInput.addEventListener('change', (): void => {
+
+  const file = fileInput.files?.[0]; 
+
+  if (!file) {
+    return;
+  }
+
+  uploadFile(file)
+
+});
+
+removeImgBtn.addEventListener('click', (event): void => {
+  event.preventDefault();
+
+  uploadIcon.classList.remove('hidden');
+  instructionText.classList.remove('hidden');
+
+  imgControlsBtns.classList.add('hidden');
+  uploadImg.classList.add('hidden');
+
+  uploadImg.src = '';
+});
+
+changeImgBtn?.addEventListener('click', (event): void => {
+  event.preventDefault();
+  fileInput.click();
+});
